ca90652ab873415127f4c5e70f01d8b7240c61cc
OverworkedCriminal <tom.swirgon@gmail.com>
2024-10-28 13:27:57 +0100

    Shader class

---
+++ src/engine/graphics/shader/Shader.hpp
@@ -0,0 +1,32 @@
+#ifndef ENGINE_GRAPHICS_SHADER_SHADER_HPP
+#define ENGINE_GRAPHICS_SHADER_SHADER_HPP
+
+#include "glad/glad.h"
+#include <expected>
+#include <string>
+
+namespace engine {
+
+class Shader {
+public:
+  static auto create(
+    GLenum shaderType,
+    std::string sourceCode
+  ) -> std::expected<Shader, std::string>;
+
+  Shader(const Shader&) = delete;
+  Shader(Shader&&);
+  ~Shader();
+
+  auto operator=(const Shader&) -> Shader& = delete;
+  auto operator=(Shader&&) -> Shader&;
+
+private:
+  Shader(GLuint shader);
+
+  GLuint m_shader;
+};
+
+}
+
+#endif
