659f583cf2023bde5e7f2a4f0a3750af5dca9c7b
OverworkedCriminal <tom.swirgon@gmail.com>
2024-10-28 18:52:01 +0100

    Program

---
+++ src/engine/graphics/shader/Program.cpp
@@ -0,0 +1,69 @@
+#include "engine/graphics/shader/Program.hpp"
+#include "engine/graphics/shader/Shader.hpp"
+#include <format>
+
+namespace engine {
+
+auto Program::create(
+  const std::vector<Shader*>& shaders
+) -> std::expected<Program, std::string> {
+  const GLuint program = glCreateProgram();
+  if (program == 0) {
+    const GLenum error = glGetError();
+    return std::unexpected(std::format("glCreateProgram error: {}", error));
+  }
+
+  for (auto shader : shaders) {
+    glAttachShader(program, **shader);
+    const GLenum error = glGetError();
+    if (error != GL_NO_ERROR) {
+      return std::unexpected(std::format("glAttachShader error: {}", error));
+    }
+  }
+
+  glLinkProgram(program);
+
+  GLint linkStatus;
+  glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+  if (linkStatus != GL_TRUE) {
+    GLint infoLogLength;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+    std::string infoLog;
+    infoLog.resize(infoLogLength);
+    glGetProgramInfoLog(program, infoLog.size(), &infoLogLength, infoLog.data());
+
+    return std::unexpected("failed to link program: " + infoLog);
+  }
+
+  Program outProgram(program);
+
+  return outProgram;  
+}
+
+Program::Program(GLuint program)
+  :m_program(program)
+{
+
+}
+
+Program::Program(Program&& other) {
+  m_program = other.m_program;
+  other.m_program = 0;
+}
+
+Program::~Program() {
+  if (m_program != 0) {
+    glDeleteProgram(m_program);
+  }
+}
+
+auto Program::operator=(Program&& other) -> Program& {
+  m_program = other.m_program;
+  other.m_program = 0;
+  return *this;
+}
+
+
+
+}
