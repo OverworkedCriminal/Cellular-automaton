22f1e92098ad710f093620a4103836734ec41d81
OverworkedCriminal <tom.swirgon@gmail.com>
2024-10-31 17:43:56 +0100

    Texture.bind

--- src/engine/graphics/texture/Texture.cpp
+++ src/engine/graphics/texture/Texture.cpp
@@ -13,11 +13,19 @@ auto Texture::create(
   }
 
   // times 3 because of RGB channels
-  const std::vector<GLfloat> initialTextureState(width * height * 3, 0.0f);
+  const std::vector<GLfloat> initialTextureState(width * height * 3, 0.5f);
 
   GLuint texture;
   glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+  const std::array<GLfloat, 4> borderColor = { 0.0f, 0.0f, 0.0f, 1.0f };
+  glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor.data());
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
   glTexImage2D(
     GL_TEXTURE_2D,
     0,
@@ -61,6 +69,11 @@ auto Texture::operator=(Texture&& other) -> Texture& {
   m_texture = other.m_texture;
   other.m_texture = 0;
   return *this;
+}
+
+auto Texture::bind() -> void {
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, m_texture);
 }
 
 }
--- src/engine/graphics/texture/Texture.hpp
+++ src/engine/graphics/texture/Texture.hpp
@@ -21,6 +21,8 @@ public:
   auto operator=(const Texture&) -> Texture& = delete;
   auto operator=(Texture&&) -> Texture&;
 
+  auto bind() -> void;
+
 private:
   Texture(GLuint texture);
 
