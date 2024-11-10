9fa55836ee960da59062efdd877e3e0050477759
OverworkedCriminal <tom.swirgon@gmail.com>
2024-11-10 15:16:39 +0100

    slightly better water simulation

--- shaders/simulation.compute.glsl
+++ shaders/simulation.compute.glsl
@@ -26,10 +26,11 @@ const uint PADDING = 1;
  * To access color use: color(cell)
  */
 const vec4 COLORS[] = vec4[](
-  vec4(1.0, 1.0, 1.0, 1.0), // PADDING 1
-  vec4(0.0, 0.0, 0.0, 1.0), // AIR     2
-  vec4(1.0, 1.0, 0.0, 1.0), // SAND    4
-  vec4(0.0, 0.0, 1.0, 1.0)  // WATER   8
+  vec4(1.0, 1.0, 1.0, 1.0), // PADDING  1
+  vec4(0.0, 0.0, 0.0, 1.0), // AIR      2
+  vec4(1.0, 1.0, 0.0, 1.0), // SAND     4
+  vec4(0.0, 0.0, 1.0, 1.0), // WATER_L  8
+  vec4(0.2, 0.2, 1.0, 1.0)  // WATER_R 16
 );
 
 /**
@@ -40,8 +41,9 @@ const vec4 COLORS[] = vec4[](
 const uint FALL_DOWN_RULES[] = uint[](
   0,  // 0b00000000 PADDING
   0,  // 0b00000000 AIR
-  10, // 0b00001010 SAND
-  2   // 0b00000010 WATER
+  26, // 0b00011010 SAND
+  2,  // 0b00000010 WATER_L
+  2   // 0b00000010 WATER_R
 );
 
 /**
@@ -53,7 +55,8 @@ const uint FALL_DIAG_RULES[] = uint[](
   0, // 0b00000000 PADDING
   0, // 0b00000000 AIR
   2, // 0b00000010 SAND
-  2  // 0b00000010 WATER
+  2, // 0b00000010 WATER_L
+  2  // 0b00000010 WATER_R
 );
 
 /**
@@ -65,7 +68,32 @@ const uint MOVE_RULES[] = uint[](
   0, // 0b00000000 PADDING
   0, // 0b00000000 AIR
   0, // 0b00000000 SAND
-  2  // 0b00000010 WATER
+  2, // 0b00000010 WATER_L
+  2  // 0b00000010 WATER_R
+);
+
+/**
+ * To access value use: moveDirection(cell)
+ * Value says in which direction cell should move
+ */
+const int MOVE_DIRECTIONS[] = int[](
+   0, // PADDING
+   0, // AIR
+   0, // SAND
+  -1, // WATER_L
+   1  // WATER_R
+);
+
+/**
+ * To access value use: moveOppositeDirectionCell(cell)
+ * Value says what is the same cell that moves in opposite direction
+ */
+const uint MOVE_OPPOSITE_DIRECTION_CELL[] = uint[](
+  1,  // PADDING
+  2,  // AIR
+  4,  // SAND
+  16, // WATER_L
+  8   // WATER_R
 );
 
 uint bufferIdx(uvec2 coords) {
@@ -90,6 +118,16 @@ uint fallDiagRule(uint cell) {
 uint moveRule(uint cell) {
   uint idx = uint(log2(cell));
   return MOVE_RULES[idx];
+}
+
+int moveDirection(uint cell) {
+  uint idx = uint(log2(cell));
+  return MOVE_DIRECTIONS[idx];
+}
+
+uint moveOppositeDirectionCell(uint cell) {
+  uint idx = uint(log2(cell));
+  return MOVE_OPPOSITE_DIRECTION_CELL[idx];
 }
 
 uint fall_down(uint idx) {
@@ -173,48 +211,89 @@ uint fall(uint idx) {
   return idx;
 }
 
-uint move_side(uint idx, uint direction) {
+uint move_side(uint idx) {
   uint otherIdx;
+  uint otherCell;
   uint mask;
 
   uint cell = inputBuffer[idx];
 
   // move into this cell
-  otherIdx = idx - direction;
-  mask = moveRule(inputBuffer[otherIdx]);
-  if ((cell & mask) > 0) {
-    // other can move (here)
-    return otherIdx;
+
+  otherIdx = idx - priorityDirection;
+  otherCell = inputBuffer[otherIdx];
+  if (moveDirection(otherCell) == priorityDirection) {
+    // otherCell moves in this direction
+    mask = moveRule(otherCell);
+    if ((cell & mask) > 0) {
+      // otherCell can move here
+      return otherIdx;
+    }
+  }
+
+  otherIdx = idx + priorityDirection;
+  otherCell = inputBuffer[otherIdx];
+  if (moveDirection(otherCell) == -priorityDirection) {
+    // otherCell moves in this direction
+    mask = moveRule(otherCell);
+    if ((cell & mask) > 0) {
+      return otherIdx;
+    }
   }
 
   // move out of this cell
 
-  otherIdx = idx + direction;
-  mask = moveRule(cell);
-  if ((inputBuffer[otherIdx] & mask) > 0) {
+  uint cellDirection = moveDirection(cell);
+  uint cellMask = moveRule(cell);
+
+  otherIdx = idx + cellDirection;
+  otherCell = inputBuffer[otherIdx];
+
+  if ((otherCell & cellMask) > 0) {
     // can move
-    return otherIdx;
+    if (cellDirection == priorityDirection) {
+      // this direction has priority
+      return otherIdx;
+    }
+
+    // this direction does not have priority
+    // It's necessary to check whether other
+    // cell has priority to move into otherIdx
+
+    uint farIdx = idx - 2 * priorityDirection;
+    uint farCell = inputBuffer[farIdx];
+    uint farDirection = moveDirection(farCell);
+    uint farMask = moveRule(farCell);
+    if ((otherCell & farMask) > 0 && farDirection == priorityDirection) {
+      // farCell can move into otherIdx AND it has priority to move into otherIdx
+      return idx;
+    } else {
+      // farCell can't move into otherIdx OR farCell does move in other side
+      return otherIdx;
+    }
   }
 
   return idx;
 }
 
 /**
- * Returns index to inputBuffer of value that should
- * be placed at idx.
- * Returns idx when value should not be changed
+ * Returns value that should be placed at idx.
  */
 uint move(uint idx) {
   uint newIdx;
 
-  newIdx = move_side(idx, priorityDirection);
+  newIdx = move_side(idx);
   if (newIdx != idx) {
     if (fall(newIdx) == newIdx) {
-      return newIdx;
+      return inputBuffer[newIdx];
+    } else {
+      return moveOppositeDirectionCell(inputBuffer[idx]);
     }
+  } else {
+    return moveOppositeDirectionCell(inputBuffer[idx]);
   }
 
-  return idx;
+  return inputBuffer[idx];
 }
 
 uint calculateCellValue(uint idx) {
@@ -230,6 +309,7 @@ uint calculateCellValue(uint idx) {
     return inputBuffer[newCellValueIdx];
   }
 
+  return move(idx);
   newCellValueIdx = move(idx);
   if (newCellValueIdx != idx) {
     return inputBuffer[newCellValueIdx];
