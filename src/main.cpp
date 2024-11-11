8fb2f95be1d5d74b12e6faa41dec9ac42acbf194
OverworkedCriminal <tom.swirgon@gmail.com>
2024-11-11 13:51:22 +0100

    simulation size independent from window size

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,16 @@ int main() {
   constexpr int WINDOW_WIDTH = 800;
   constexpr int WINDOW_HEIGHT = 600;
 
+  constexpr int SIMULATION_WIDTH = 100;
+  constexpr int SIMULATION_HEIGHT = 100;
+
   const engine::Config config = {
     .windowTitle = "Cellular automaton",
     .windowWidth = WINDOW_WIDTH,
     .windowHeight = WINDOW_HEIGHT
   };
 
-  auto simulation = GpuSimulation::create(WINDOW_WIDTH, WINDOW_HEIGHT);
+  auto simulation = GpuSimulation::create(SIMULATION_WIDTH, SIMULATION_HEIGHT);
   if (!simulation.has_value()) {
     std::cerr << "Simulation creation failed\n\t" << simulation.error() << '\n';
     return -1;
