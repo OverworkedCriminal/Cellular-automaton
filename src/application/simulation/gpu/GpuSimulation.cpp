da6787d9fceb884496caf51f553f07b23d897f37
OverworkedCriminal <tom.swirgon@gmail.com>
2024-11-24 16:40:05 +0100

    adjust GpuSimulation to all previous changes

--- src/application/simulation/gpu/GpuSimulation.cpp
+++ src/application/simulation/gpu/GpuSimulation.cpp
@@ -1,15 +1,20 @@
 #include "application/simulation/gpu/GpuSimulation.hpp"
+#include "application/drawing/TextureDrawingProgram.hpp"
+#include "application/simulation/Cell.hpp"
+#include "application/simulation/painting/painting.hpp"
 #include "engine/error/Error.hpp"
 #include "engine/graphics/shader/Program.hpp"
 #include "engine/graphics/shader/Shader.hpp"
 #include "engine/graphics/shader/ShaderStorageBuffer.hpp"
+#include "engine/graphics/texture/Texture.hpp"
 #include "engine/utils/error.hpp"
+#include <ctime>
 #include <iostream>
 
 using engine::error;
 using engine::errorGL;
 
-static constexpr int PADDING = 2;
+static constexpr unsigned int PADDING = 2;
 
 static auto isGpuBigEndian() -> std::expected<bool, engine::Error> {
   auto shaderResult = engine::Shader::create_from_file(GL_COMPUTE_SHADER, "shaders/endianess.compute.glsl");
@@ -42,7 +47,7 @@ static auto isGpuBigEndian() -> std::expected<bool, engine::Error> {
     return std::unexpected(error("failed to bind endianess shader buffer", bindBufferResult.error()));
   }
 
-  std::vector<std::byte> shaderBufferBytes(SHADER_BUFFER_SIZE, std::byte(0));
+  std::vector<uint8_t> shaderBufferBytes(SHADER_BUFFER_SIZE, 0);
   shaderBuffer.store(shaderBufferBytes);
 
   // start compute shader
@@ -56,8 +61,8 @@ static auto isGpuBigEndian() -> std::expected<bool, engine::Error> {
   // Shader stores value 1 to shaderBuffer on 4 bytes.
   // If GPU uses little endian value 1 should be at byteIdx 0
   // If GPU uses big endian value 1 should be at byteIdx 3
-  std::byte byte = shaderBufferBytes[3];
-  bool isBigEndian = byte == std::byte(1);
+  uint8_t byte = shaderBufferBytes[3];
+  bool isBigEndian = byte == 1;
 
   return isBigEndian;
 }
@@ -78,53 +83,36 @@ GpuSimulation::GpuSimulation(int width, int height)
   ,m_height(height)
 {}
 
-auto GpuSimulation::width() -> unsigned int {
-  return m_width;
-}
-
-auto GpuSimulation::height() -> unsigned int {
-  return m_height;
-}
-
-auto GpuSimulation::padding() -> unsigned int {
-  return PADDING;
-}
-
-auto GpuSimulation::paint(const std::function<void(SimulationGrid&)>& paintFn) -> void {
-  m_inputSSBO->load(*m_buffer);
-
-  paintFn(*m_simulationGrid);
-
-  m_inputSSBO->store(*m_buffer);
-}
-
 auto GpuSimulation::onCreate(const Context& applicationContext) -> std::expected<void, engine::Error> {
-  auto isGpuBigEndianResult = isGpuBigEndian();
-  if (!isGpuBigEndianResult.has_value()) {
-    return std::unexpected(error("failed to check if gpu is big endian", isGpuBigEndianResult.error()));
-  }
-
-  auto initSimulationBufferResult = initSimulationGrid(*isGpuBigEndianResult);
-  if (!initSimulationBufferResult.has_value()) {
-    return std::unexpected(error("failed to init simulation buffer", initSimulationBufferResult.error()));
-  }
+  srand(time(NULL));
 
   auto initSimulationResult = initSimulation();
   if (!initSimulationResult.has_value()) {
     return std::unexpected(error("failed to init simulation", initSimulationResult.error()));
   }
 
+  auto initDrawingResult = initDrawing();
+  if (!initDrawingResult.has_value()) {
+    return std::unexpected(error("failed to init drawing", initDrawingResult.error()));
+  }
+
   return {};
 }
 
 auto GpuSimulation::onUpdate(const Context& applicationContext) -> std::expected<void, engine::Error> {
+  if (applicationContext.mouseLeftPressed) {
+    const Cell cell = tryMapIntoCell(applicationContext.keyboardLastKeyPressed)
+      .value_or(Cell::AIR);
+    paint(cell, applicationContext);
+  }
+
   auto useSimulationProgramResult = m_simulationProgram->useProgram();
   if (!useSimulationProgramResult.has_value()) {
     return std::unexpected(error("failed to use simulation program", useSimulationProgramResult.error()));
   }
   auto uniformResult = m_simulationProgram->setUniform(
     "priorityDirection",
-    static_cast<GLint>(applicationContext.priorityDirection)
+    (rand() % 2) * 2 - 1
   );
   if (!uniformResult.has_value()) {
     std::cerr << "failed to set priorityDirection uniform\n\t" << uniformResult.error() << '\n';
@@ -138,7 +126,7 @@ auto GpuSimulation::onUpdate(const Context& applicationContext) -> std::expected
   if (!outputBindResult.has_value()) {
     return std::unexpected(error("failed to bind output SSBO", outputBindResult.error()));
   }
-  auto bindImageTextureResult = applicationContext.texturePtr->bindImageTexture();
+  auto bindImageTextureResult = m_drawingTexture->bindImageTexture(0);
   if (!bindImageTextureResult.has_value()) {
     return std::unexpected(error("failed to bind image texture", bindImageTextureResult.error()));
   }
@@ -156,50 +144,38 @@ auto GpuSimulation::onUpdate(const Context& applicationContext) -> std::expected
     return std::unexpected(errorGL("glMemoryBarrier", glError));
   }
 
+  auto drawResult = m_drawingProgram->draw(*m_drawingTexture);
+  if (!drawResult.has_value()) {
+    return std::unexpected(error("failed to draw texture", drawResult.error()));
+  }
+
   std::swap(*m_inputSSBO, *m_outputSSBO);
 
   return {};
 }
 
-auto GpuSimulation::initSimulationGrid(bool isGpuBigEndian) -> std::expected<void, engine::Error> {
-  m_buffer = std::move(std::vector<std::byte>(m_width * m_height * 4, std::byte(0)));
-
-  // GPU simulation values are stored on 4 byte uints.
-  // Using single bytes would cause too many data races.
-  auto simulationGridResult = SimulationGrid::create(
-    &*m_buffer,
-    m_width,
-    m_height,
-    4,
-    3 * isGpuBigEndian
-  );
-  if (!simulationGridResult.has_value()) {
-    return std::unexpected(error("failed to create simulation grid", simulationGridResult.error()));
+auto GpuSimulation::initSimulation() -> std::expected<void, engine::Error> {
+  auto isGpuBigEndianResult = isGpuBigEndian();
+  if (!isGpuBigEndianResult.has_value()) {
+    return std::unexpected(error("failed to check if gpu is big endian", isGpuBigEndianResult.error()));
   }
-  m_simulationGrid = std::move(*simulationGridResult);
+  m_bufferValueStride = 4;
+  m_bufferValueOffset = 3 * *isGpuBigEndianResult;
 
-  // Fill grid with ones
-  // Frame of size PADDING is filled with 0 to make sure they don't take part in simulation
-  for (int col = 0; col < m_width; ++col) {
-    for (int row = 0; row < m_height; ++row) {
-      if (col < PADDING || col >= m_width - PADDING || row < PADDING || row >= m_height - PADDING) {
-        m_simulationGrid->setCell(col, row, std::byte(1) /*PADDING*/);
+  m_buffer = std::vector<Cell>(m_width * m_height * 4, static_cast<Cell>(0));
+  auto& buffer = reinterpret_cast<std::vector<uint8_t>&>(*m_buffer);
+
+  for (int row = 0; row < m_height; ++row) {
+    for (int col = 0; col < m_width; ++col) {
+      int idx = (row * m_width + col) * m_bufferValueStride + m_bufferValueOffset;
+      if (row < PADDING || row >= m_width - PADDING || col < PADDING || col >= m_width - PADDING) {
+        buffer[idx] = static_cast<uint8_t>(Cell::PADDING);
       } else {
-        m_simulationGrid->setCell(col, row, std::byte(2) /*AIR*/);
+        buffer[idx] = static_cast<uint8_t>(Cell::AIR);
       }
     }
   }
 
-  return {};
-}
-
-/**
- * @brief
- * Relies on m_buffer so should be called after initSimulationGrid
- * 
- * @return std::expected<void, engine::Error> 
- */
-auto GpuSimulation::initSimulation() -> std::expected<void, engine::Error> {
   auto simulationShaderResult = engine::Shader::create_from_file(GL_COMPUTE_SHADER, "shaders/simulation.compute.glsl");
   if (!simulationShaderResult.has_value()) {
     return std::unexpected(error("failed to create compute shader", simulationShaderResult.error()));
@@ -217,18 +193,17 @@ auto GpuSimulation::initSimulation() -> std::expected<void, engine::Error> {
     return std::unexpected(error("failed to create input SSBO", inputSSBO.error()));
   }
   m_inputSSBO = std::move(*inputSSBO);
-  m_inputSSBO->store(*m_buffer);
+  m_inputSSBO->store(buffer);
 
   auto outputSSBO = engine::ShaderStorageBuffer::create(m_width * m_height * 4);
   if (!outputSSBO.has_value()) {
     return std::unexpected(error("failed to create output SSBO", outputSSBO.error()));
   }
   m_outputSSBO = std::move(*outputSSBO);
-  m_outputSSBO->store(*m_buffer);
+  m_outputSSBO->store(buffer);
 
-  constexpr GLuint GRID_PADDING = 2;
-  m_computeSpaceX = m_width - 2 * GRID_PADDING;
-  m_computeSpaceY = m_height - 2 * GRID_PADDING;
+  m_computeSpaceX = m_width - 2 * PADDING;
+  m_computeSpaceY = m_height - 2 * PADDING;
 
   auto useProgramResult = m_simulationProgram->useProgram();
   if (!useProgramResult.has_value()) {
@@ -244,7 +219,7 @@ auto GpuSimulation::initSimulation() -> std::expected<void, engine::Error> {
   if (!uniformResult.has_value()) {
     std::cerr << "failed to set uniform gridHeight\n\t" << uniformResult.error() << '\n';
   }
-  uniformResult = m_simulationProgram->setUniform("gridPadding", GRID_PADDING);
+  uniformResult = m_simulationProgram->setUniform("gridPadding", PADDING);
   if (!uniformResult.has_value()) {
     std::cerr << "failed to set uniform gridPadding\n\t" << uniformResult.error() << '\n';
   }
@@ -254,4 +229,46 @@ auto GpuSimulation::initSimulation() -> std::expected<void, engine::Error> {
   }
 
   return {};
+}
+
+auto GpuSimulation::initDrawing() -> std::expected<void, engine::Error> {
+  auto texture = engine::Texture::create(m_width, m_height);
+  if (!texture.has_value()) {
+    return std::unexpected(error("failed to create texture", texture.error()));
+  }
+  m_drawingTexture = std::move(*texture);
+
+  auto drawingProgram = TextureDrawingProgram::create();
+  if (!drawingProgram.has_value()) {
+    return std::unexpected(error("failed to create drawing program", drawingProgram.error()));
+  }
+  m_drawingProgram = std::move(*drawingProgram);
+
+  return {};
+}
+
+auto GpuSimulation::paint(Cell cell, const Context& applicationContext) -> void {
+  auto& buffer = reinterpret_cast<std::vector<uint8_t>&>(*m_buffer);
+
+  m_inputSSBO->load(buffer);
+
+  auto scaleX = static_cast<float>(m_width) / static_cast<float>(applicationContext.framebufferWidth);
+  auto scaleY = static_cast<float>(m_height) / static_cast<float>(applicationContext.framebufferHeight);
+
+  auto posX = static_cast<unsigned int>(static_cast<float>(applicationContext.mousePosX) * scaleX);
+  auto posY = static_cast<unsigned int>(static_cast<float>(applicationContext.framebufferHeight - 1 - applicationContext.mousePosY) * scaleY);
+
+  ::paint(
+    posX,
+    posY,
+    cell,
+    *m_buffer,
+    m_width,
+    m_height,
+    PADDING,
+    m_bufferValueOffset,
+    m_bufferValueStride
+  );
+
+  m_inputSSBO->store(buffer);
 }
--- src/application/simulation/gpu/GpuSimulation.hpp
+++ src/application/simulation/gpu/GpuSimulation.hpp
@@ -1,10 +1,13 @@
 #ifndef APPLICATION_SIMULATION_GPU_GPU_SIMULATION_HPP
 #define APPLICATION_SIMULATION_GPU_GPU_SIMULATION_HPP
 
+#include "application/drawing/TextureDrawingProgram.hpp"
+#include "application/simulation/Cell.hpp"
 #include "application/simulation/ISimulation.hpp"
 #include "engine/error/Error.hpp"
 #include "engine/graphics/shader/Program.hpp"
 #include "engine/graphics/shader/ShaderStorageBuffer.hpp"
+#include "engine/graphics/texture/Texture.hpp"
 #include <expected>
 
 class GpuSimulation :public ISimulation {
@@ -20,32 +23,32 @@ public:
   auto operator=(const GpuSimulation&) -> GpuSimulation& = delete;
   auto operator=(GpuSimulation&&) -> GpuSimulation& = default;
 
-  auto width() -> unsigned int override;
-  auto height() -> unsigned int override;
-  auto padding() -> unsigned int override;
-
-  auto paint(const std::function<void(SimulationGrid&)>& paintFn) -> void override;
   auto onCreate(const Context& applicationContext) -> std::expected<void, engine::Error> override;
   auto onUpdate(const Context& applicationContext) -> std::expected<void, engine::Error> override;
 
 private:
   GpuSimulation(int width, int height);
 
-  auto initSimulationGrid(bool isGpuBigEndian) -> std::expected<void, engine::Error>;
   auto initSimulation() -> std::expected<void, engine::Error>;
+  auto initDrawing() -> std::expected<void, engine::Error>;
+
+  auto paint(Cell cell, const Context& applicationContext) -> void;
 
   unsigned int m_width;
   unsigned int m_height;
 
-  int m_computeSpaceX;
-  int m_computeSpaceY;
-
-  std::optional<std::vector<std::byte>> m_buffer;
-  std::optional<SimulationGrid> m_simulationGrid;
+  std::optional<std::vector<Cell>> m_buffer;
+  unsigned int m_bufferValueOffset;
+  unsigned int m_bufferValueStride;
 
   std::optional<engine::Program> m_simulationProgram;
   std::optional<engine::ShaderStorageBuffer> m_inputSSBO;
   std::optional<engine::ShaderStorageBuffer> m_outputSSBO;
+  int m_computeSpaceX;
+  int m_computeSpaceY;
+
+  std::optional<engine::Texture> m_drawingTexture;
+  std::optional<TextureDrawingProgram> m_drawingProgram;
 };
 
 #endif
