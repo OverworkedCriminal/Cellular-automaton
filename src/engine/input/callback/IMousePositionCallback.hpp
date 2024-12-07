76c5047d503f09420603b8fa708bc3532e71d001
OverworkedCriminal <tom.swirgon@gmail.com>
2024-12-07 14:47:23 +0100

    MousePosition struct

--- src/engine/input/callback/IMousePositionCallback.hpp
+++ src/engine/input/callback/IMousePositionCallback.hpp
@@ -1,13 +1,15 @@
 #ifndef ENGINE_INPUT_CALLBACK_IMOUSE_POSITION_CALLBACK_HPP
 #define ENGINE_INPUT_CALLBACK_IMOUSE_POSITION_CALLBACK_HPP
 
+#include "engine/input/MousePosition.hpp"
+
 namespace engine::input {
 
 class IMousePositionCallback {
 public:
   virtual ~IMousePositionCallback() {}
 
-  virtual auto onPositionEvent(unsigned posX, unsigned posY) -> void;
+  virtual auto onPositionEvent(MousePosition position) -> void;
 };
 
 }
