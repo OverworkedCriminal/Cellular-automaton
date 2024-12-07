050b5b4a6637916f1ae6209658592bb11e4c13a8
OverworkedCriminal <tom.swirgon@gmail.com>
2024-12-07 17:55:38 +0100

    replace removed Context with EngineContext in IApplication

--- src/engine/application/IApplication.hpp
+++ src/engine/application/IApplication.hpp
@@ -1,7 +1,7 @@
 #ifndef ENGINE_APPLICATION_IAPPLICATION_HPP
 #define ENGINE_APPLICATION_IAPPLICATION_HPP
 
-#include "engine/Context.hpp"
+#include "engine/EngineContext.hpp"
 #include "engine/error/Error.hpp"
 #include <expected>
 
@@ -11,9 +11,9 @@ class IApplication {
 public:
   virtual ~IApplication() {}
 
-  virtual auto onCreate(const Context& context) -> std::expected<void, Error> { return {}; }
-  virtual auto onDestroy(const Context& context) -> std::expected<void, Error> { return {}; }
-  virtual auto onUpdate(const Context& context) -> std::expected<void, Error> { return {}; }
+  virtual auto onCreate(EngineContext& context) -> std::expected<void, Error> { return {}; }
+  virtual auto onDestroy(EngineContext& context) -> std::expected<void, Error> { return {}; }
+  virtual auto onUpdate(EngineContext& context) -> std::expected<void, Error> { return {}; }
 };
 
 } // namespace engine
