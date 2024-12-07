a322800a9bc0ba51ccbfd6c6d46166001b953dad
OverworkedCriminal <tom.swirgon@gmail.com>
2024-12-07 13:03:08 +0100

    move KeyboardKey to engine/input/binding

---
+++ src/engine/input/binding/KeyboardKey.hpp
@@ -0,0 +1,18 @@
+#ifndef ENGINE_INPUT_BINDING_KEYBOARD_KEY_HPP
+#define ENGINE_INPUT_BINDING_KEYBOARD_KEY_HPP
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+namespace engine::input {
+
+enum class KeyboardKey {
+  _1 = GLFW_KEY_1,
+  _2 = GLFW_KEY_2,
+  _3 = GLFW_KEY_3,
+  _
+};
+
+}
+
+#endif
