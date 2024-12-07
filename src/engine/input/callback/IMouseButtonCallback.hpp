7ea23d3246ae78fa92514beaf798d038cb92cf73
OverworkedCriminal <tom.swirgon@gmail.com>
2024-12-07 14:37:41 +0100

    input callback interfaces

---
+++ src/engine/input/callback/IMouseButtonCallback.hpp
@@ -0,0 +1,17 @@
+#ifndef ENGINE_INPUT_CALLBACK_IMOUSE_BUTTON_CALLBACK_HPP
+#define ENGINE_INPUT_CALLBACK_IMOUSE_BUTTON_CALLBACK_HPP
+
+#include "engine/input/binding/MouseButton.hpp"
+
+namespace engine::input {
+
+class IMouseButtonCallback {
+public:
+  virtual ~IMouseButtonCallback() {}
+
+  virtual auto onButtonEvent(MouseButton button, bool pressed) -> void;
+};
+
+}
+
+#endif
