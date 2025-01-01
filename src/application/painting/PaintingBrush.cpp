d11401b3e25be9425d4f996f89118aa01aced4e3
OverworkedCriminal <tom.swirgon@gmail.com>
2025-01-01 11:12:47 +0100

    Update PaintingBrush implementation to further simplify api

--- src/application/painting/PaintingBrush.cpp
+++ src/application/painting/PaintingBrush.cpp
@@ -1,71 +1,48 @@
 #include "application/painting/PaintingBrush.hpp"
+#include "application/painting/PaintingCanvasDescription.hpp"
 #include "engine/utils/dto/Position2D.hpp"
 #include "engine/utils/dto/Size2D.hpp"
 #include <algorithm>
 
-using engine::Size2D;
 using engine::Position2D;
 
-auto PaintingBrush::create(
-  Size2D<uint32_t> simulationSize,
-  uint32_t simulationPadding,
-  Size2D<uint32_t> framebufferSize,
-  uint32_t canvasValueOffset,
-  uint32_t canvasValueStride
-) -> PaintingBrush {
+auto PaintingBrush::create(uint8_t size, uint8_t value) -> PaintingBrush {
   return PaintingBrush(
-    simulationSize,
-    simulationPadding,
-    framebufferSize,
-    canvasValueOffset,
-    canvasValueStride
+    size,
+    value
   );
 }
 
-PaintingBrush::PaintingBrush(
-  Size2D<uint32_t> simulationSize,
-  uint32_t simulationPadding,
-  Size2D<uint32_t> framebufferSize,
-  uint32_t canvasValueOffset,
-  uint32_t canvasValueStride
-)
-  :m_brushValue(0)
-  ,m_brushSize(1)
-  ,m_simulationSize(simulationSize)
-  ,m_simulationPadding(simulationPadding)
-  ,m_framebufferSize(framebufferSize)
-  ,m_canvasValueOffset(canvasValueOffset)
-  ,m_canvasValueStride(canvasValueStride)
+PaintingBrush::PaintingBrush(uint8_t size, uint8_t value)
+  :m_brushValue(value)
+  ,m_brushSize(size)
 {}
 
 auto PaintingBrush::paint(
   std::vector<uint8_t>& canvas,
-  Position2D<uint32_t> mousePosition
+  PaintingCanvasDescription& canvasDescription,
+  Position2D<uint32_t> position
 ) const -> void {
-  const auto [simulationWidth, simulationHeight] = m_simulationSize;
-  const auto [framebufferWidth, framebufferHeight] = m_framebufferSize;
-  const auto [mousePosX, mousePosY] = mousePosition;
+  const auto [size, padding, valueOffset, valueStride] = canvasDescription;
+  const auto [posX, posY] = position;
 
-  const uint32_t mouseScaledPosX = (static_cast<float>(simulationWidth) / framebufferWidth) * mousePosX;
-  const uint32_t mouseScaledPosY = (static_cast<float>(simulationHeight) / framebufferHeight) * mousePosY;
+  const int32_t lowerBoundX = padding;
+  const int32_t upperBoundX = size.width - padding;
 
-  const int32_t lBoundX = m_simulationPadding;
-  const int32_t uBoundX = simulationWidth - m_simulationPadding;
-
-  const int32_t lBoundY = m_simulationPadding;
-  const int32_t uBoundY = simulationHeight - m_simulationPadding;
+  const int32_t lowerBoundY = padding;
+  const int32_t upperBoundY = size.height - padding;
 
   const int32_t radius = m_brushSize - 1;
 
   for (int32_t row = -radius; row <= radius; ++row) {
     for (int32_t col = -radius; col <= radius; ++col) {
-      int32_t x = mouseScaledPosX + col;
-      int32_t y = mouseScaledPosY + row;
-      if (x < lBoundX || x >= uBoundX || y < lBoundY || y >= uBoundY) {
+      const int32_t x = posX + col;
+      const int32_t y = posY + row;
+      if (x < lowerBoundX || x >= upperBoundX || y < lowerBoundY || y >= upperBoundY) {
         continue;
       }
 
-      int32_t idx = (y * simulationWidth + x) * m_canvasValueStride + m_canvasValueOffset;
+      const int32_t idx = (y * size.width + x) * valueStride + valueOffset;
       canvas[idx] = m_brushValue;
     }
   }
@@ -81,8 +58,4 @@ auto PaintingBrush::setSize(uint8_t size) -> void {
 
 auto PaintingBrush::getSize() const -> uint8_t {
   return m_brushSize;
-}
-
-auto PaintingBrush::setFramebufferSize(Size2D<uint32_t> size) -> void {
-  m_framebufferSize = size;
 }
--- src/application/painting/PaintingBrush.hpp
+++ src/application/painting/PaintingBrush.hpp
@@ -1,8 +1,8 @@
 #ifndef APPLICATION_PAINTING_PAINTING_BRUSH_HPP
 #define APPLICATION_PAINTING_PAINTING_BRUSH_HPP
 
+#include "application/painting/PaintingCanvasDescription.hpp"
 #include "engine/utils/dto/Position2D.hpp"
-#include "engine/utils/dto/Size2D.hpp"
 #include <cstdint>
 #include <vector>
 
@@ -11,27 +11,7 @@
  */
 class PaintingBrush {
 public:
-
-  /**
-   * @brief Constructor
-   * 
-   * By default brushValue is set to 0.
-   * By default brushSize is set to 1.
-   * 
-   * @param simulationSize 
-   * @param simulationPadding 
-   * @param framebufferSize 
-   * @param canvasValueOffset 
-   * @param canvasValueStride 
-   * @return PaintingBrush 
-   */
-  static auto create(
-    engine::Size2D<uint32_t> simulationSize,
-    uint32_t simulationPadding,
-    engine::Size2D<uint32_t> framebufferSize,
-    uint32_t canvasValueOffset,
-    uint32_t canvasValueStride
-  ) -> PaintingBrush;
+  static auto create(uint8_t size, uint8_t value) -> PaintingBrush;
 
   PaintingBrush(const PaintingBrush&) = default;
   PaintingBrush(PaintingBrush&&) = default;
@@ -39,36 +19,31 @@ public:
   auto operator=(const PaintingBrush&) -> PaintingBrush& = default;
   auto operator=(PaintingBrush&&) -> PaintingBrush& = default;
 
+  /**
+   * @brief paint selected value onto canvas
+   * 
+   * @param canvas 
+   * @param canvasDescription 
+   * @param position (in simulation space)
+   */
   auto paint(
     std::vector<uint8_t>& canvas,
-    engine::Position2D<uint32_t> mousePosition
+    PaintingCanvasDescription& canvasDescription,
+    engine::Position2D<uint32_t> position
   ) const -> void;
 
   auto setValue(uint8_t value) -> void;
   auto setSize(uint8_t size) -> void;
   auto getSize() const -> uint8_t;
-  auto setFramebufferSize(engine::Size2D<uint32_t> size) -> void;
 
 private:
-  PaintingBrush(
-    engine::Size2D<uint32_t> simulationSize,
-    uint32_t simulationPadding,
-    engine::Size2D<uint32_t> framebufferSize,
-    uint32_t canvasValueOffset,
-    uint32_t canvasValueStride
-  );
+  PaintingBrush(uint8_t size, uint8_t value);
 
-  // value that will be used to update canvas
+  /**
+   * @brief value that will be used to update canvas
+   */
   uint8_t m_brushValue;
   uint8_t m_brushSize;
-
-  engine::Size2D<uint32_t> m_simulationSize;
-  uint32_t m_simulationPadding;
-
-  engine::Size2D<uint32_t> m_framebufferSize;
-
-  uint32_t m_canvasValueOffset;
-  uint32_t m_canvasValueStride;
 };
 
 #endif
