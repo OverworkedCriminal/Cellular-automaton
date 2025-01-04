59d63107742f079c8e94badb2a2d8cff973db675
OverworkedCriminal <tom.swirgon@gmail.com>
2025-01-04 11:35:57 +0100

    cpu-simulator falling straight test

--- src/application/painting/PaintingBrush.cpp
+++ src/application/painting/PaintingBrush.cpp
@@ -1,5 +1,6 @@
 #include "application/painting/PaintingBrush.hpp"
 #include "application/painting/PaintingCanvasDescription.hpp"
+#include "application/painting/painting.hpp"
 #include "engine/utils/dto/Position2D.hpp"
 #include "engine/utils/dto/Size2D.hpp"
 #include <algorithm>
@@ -45,7 +46,14 @@ auto PaintingBrush::paint(
         continue;
       }
 
-      const int32_t idx = (y * size.width + x) * valueStride + valueOffset;
+      const uint32_t idx = mapPositionToCanvasIndex(
+        { 
+          .x = static_cast<uint32_t>(x),
+          .y = static_cast<uint32_t>(y)
+        },
+        canvasDescription
+      );
+
       canvas[idx] = m_brushValue;
     }
   }
--- src/application/simulation/cpu/CpuSimulator.hpp
+++ src/application/simulation/cpu/CpuSimulator.hpp
@@ -2,6 +2,7 @@
 #define APPLICATION_SIMULATION_CPU_CPU_SIMULATOR_HPP
 
 #include "engine/error/Error.hpp"
+#include "engine/utils/dto/Size2D.hpp"
 #include <cstdint>
 #include <expected>
 #include <vector>
@@ -15,17 +16,13 @@ public:
   /**
    * @brief Class constructor
    * 
-   * @param width (does not include padding)
-   * @param height (does not include padding)
+   * @param size (does not include padding)
    *
    * @return std::expected<CpuSimulator, engine::Error>
    *
    * @throws engine::Error when width or height is less than 1
    */
-  static auto create(
-    uint32_t width,
-    uint32_t height
-  ) -> std::expected<CpuSimulator, engine::Error>;
+  static auto create(engine::Size2D<uint32_t> size) -> std::expected<CpuSimulator, engine::Error>;
 
   CpuSimulator(const CpuSimulator&) = delete;
   CpuSimulator(CpuSimulator&&) = default;
@@ -36,21 +33,16 @@ public:
   auto run(
     const std::vector<uint8_t>& bufferIn,
     std::vector<uint8_t>& bufferOut
-  ) -> void;
+  ) const -> void;
 
 private:
   CpuSimulator(
-    uint32_t width,
-    uint32_t height,
-    uint32_t widthWithPadding,
-    uint32_t heightWithPadding
+    engine::Size2D<uint32_t> size,
+    engine::Size2D<uint32_t> sizeWithPadding
   );
 
-  uint32_t m_width;
-  uint32_t m_height;
-  uint32_t m_widthWithPadding;
-  uint32_t m_heightWithPadding;
-
+  engine::Size2D<uint32_t> m_size;
+  engine::Size2D<uint32_t> m_sizeWithPadding;
 };
 
 #endif
