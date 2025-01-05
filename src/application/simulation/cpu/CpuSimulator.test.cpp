05f92f90abd913e9c55afce3c91557b98f3a5ca8
OverworkedCriminal <tom.swirgon@gmail.com>
2025-01-05 11:24:17 +0100

    extract cpu-simulator test fixture for later use

--- src/application/simulation/cpu/CpuSimulator.test.cpp
+++ src/application/simulation/cpu/CpuSimulator.test.cpp
@@ -4,11 +4,13 @@
 #include "application/simulation/cell.hpp"
 #include "application/simulation/padding.hpp"
 #include "engine/utils/dto/Position2D.hpp"
+#include "engine/utils/dto/Size2D.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <vector>
 
 using std::vector;
 using engine::Position2D;
+using engine::Size2D;
 
 TEST_CASE("Invalid width", "[constructor]") {
   const auto simulator = CpuSimulator::create({ .width = 0, .height = 1 });
@@ -20,22 +22,26 @@ TEST_CASE("Invalid height", "[constructor]") {
   REQUIRE_FALSE(simulator.has_value());
 }
 
-class FallStraightDownFixture {
-public:
-  FallStraightDownFixture()
-    :simulator(*CpuSimulator::create({ .width = 1, .height = 2 }))
-    ,canvasDescription({
+struct TestFixture {
+  CpuSimulator simulator;
+  PaintingCanvasDescription canvasDescription;
+  vector<uint8_t> bufferIn;
+  vector<uint8_t> bufferOut;
+
+  static auto create(Size2D<uint32_t> simulationSize) -> TestFixture {
+    CpuSimulator simulator = *CpuSimulator::create(simulationSize);
+    PaintingCanvasDescription canvasDescription = {
       .size = {
-        .width = 1 + 2 * PADDING_SIZE,
-        .height = 2 + 2 * PADDING_SIZE
+        .width = simulationSize.width + 2 * PADDING_SIZE,
+        .height = simulationSize.height + 2 * PADDING_SIZE
       },
       .paddingSize = PADDING_SIZE,
       .valueOffset = 0,
       .valueStride = 1
-    })
-    ,bufferIn(canvasDescription.size.width * canvasDescription.size.height * canvasDescription.valueStride, 0)
-    ,bufferOut(canvasDescription.size.width * canvasDescription.size.height * canvasDescription.valueStride, 0)
-  {
+    };
+    vector<uint8_t> bufferIn(canvasDescription.size.width * canvasDescription.size.height * canvasDescription.valueStride, 0);
+    vector<uint8_t> bufferOut(canvasDescription.size.width * canvasDescription.size.height * canvasDescription.valueStride, 0);
+
     for (uint32_t row = 0; row < canvasDescription.size.height; ++row) {
       for (uint32_t col = 0; col < canvasDescription.size.width; ++col) {
         const uint32_t idx = mapSimulationPositionToCanvasIndex(
@@ -49,13 +55,19 @@ public:
         bufferOut[idx] = cell::PADDING;
       }
     }
-  }
 
+    return {
+      .simulator = std::move(simulator),
+      .canvasDescription = std::move(canvasDescription),
+      .bufferIn = std::move(bufferIn),
+      .bufferOut = std::move(bufferOut)
+    };
+  }
+};
+
+class FallStraightDownFixture {
 protected:
-  CpuSimulator simulator;
-  PaintingCanvasDescription canvasDescription;
-  vector<uint8_t> bufferIn;
-  vector<uint8_t> bufferOut;
+  TestFixture fixture = TestFixture::create({ .width = 1, .height = 2 });
 
   /**
    * @brief Parametrized test that checks falling straight down
@@ -67,6 +79,8 @@ protected:
    * @return false when cells didn't swap position
    */
   auto fallStraightDown(uint8_t topCell, uint8_t botCell) -> bool {
+    auto& [simulator, canvasDescription, bufferIn, bufferOut] = fixture;
+
     const Position2D<uint32_t> topPosition = { .x = 0 + PADDING_SIZE, .y = 1 + PADDING_SIZE };
     const Position2D<uint32_t> botPosition = { .x = 0 + PADDING_SIZE, .y = 0 + PADDING_SIZE };
     const uint32_t topIndex = mapSimulationPositionToCanvasIndex(topPosition, canvasDescription);
