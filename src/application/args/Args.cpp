5395f985a2faf6af297f393e59e4d4a3d01dd935
OverworkedCriminal <tom.swirgon@gmail.com>
2025-01-24 12:42:41 +0100

    update Args parsing implementation

--- src/application/args/Args.cpp
+++ src/application/args/Args.cpp
@@ -16,121 +16,164 @@ using std::string;
 using std::expected;
 using std::unexpected;
 
-static auto parseIntValue(
-  int argc,
-  const char** argv,
-  uint32_t i
-) -> expected<int, Error> {
-  if (i >= argc) {
-    return unexpected(error("missing value"));
-  }
+enum class State : uint8_t {
+  PARSING_OPTION,
+  PARSING_VALUE,
+  PARSING_OPTIONAL_VALUE
+};
 
-  int value = std::atoi(argv[i]);
-  if (value == 0) {
-    return unexpected(error("invalid value"));
+struct Context {
+  /**
+   * Parser state
+   */
+  State state;
+
+  /**
+   * Arguments parsed by the parser
+   */
+  Args* args;
+
+  /**
+   * Pointer to value for parsing value
+   */
+  uint32_t* valuePtr;
+};
+
+static auto parseUint32(const char* str) -> expected<uint32_t, Error> {
+  uint32_t value = 0;
+  uint32_t i = 0;
+
+  while (str[i] != '\0') {
+    if (str[i] < '0' || str[i] > '9') {
+      return unexpected(error("invalid uint32_t character"));
+    }
+
+    value = value * 10 + (str[i] - '0');
+    i += 1;
   }
 
   return value;
+}
+
+static auto printHelp() -> void {
+  std::cout << "cellular-automaton -w <width> -h <height>\n"
+            << "\nARGS:\n"
+            << '\t' << std::left << std::setw(17) << "--cpu [coreCount]" << " run simulation on CPU (default). Default coreCount is 1\n"
+            << '\t' << std::left << std::setw(17) << "--gpu" << " run simulation on GPU\n"
+            << '\t' << std::left << std::setw(17) << "-w, --width" << " simulation width\n"
+            << '\t' << std::left << std::setw(17) << "-h, --height" << " simulation height\n"
+            << '\t' << std::left << std::setw(17) << "--window-width" << " simulation width\n"
+            << '\t' << std::left << std::setw(17) << "--window-height" << " simulation height\n"
+            << '\t' << std::left << std::setw(17) << "--help" << " display this message\n";
+}
+
+static auto parseOption(Context& context, const char* option) -> expected<void, Error> {
+  if (std::strcmp(option, "--cpu") == 0) {
+    context.args->processor = Processor::CPU;
+    context.state = State::PARSING_OPTIONAL_VALUE;
+    context.valuePtr = &context.args->processorCoreCount;
+    return {};
+  }
+  if (std::strcmp(option, "--gpu") == 0) {
+    context.args->processor = Processor::GPU;
+    return {};
+  }
+
+  if (std::strcmp(option, "-w") == 0 || std::strcmp(option, "--width") == 0) {
+    context.state = State::PARSING_VALUE;
+    context.valuePtr = &context.args->widthSimulation;
+    return {};
+  }
+  if (std::strcmp(option, "-h") == 0 || std::strcmp(option, "--height") == 0) {
+    context.state = State::PARSING_VALUE;
+    context.valuePtr = &context.args->heightSimulation;
+    return {};
+  }
+
+  if (std::strcmp(option, "--window-width") == 0) {
+    context.state = State::PARSING_VALUE;
+    context.valuePtr = &context.args->widthWindow;
+    return {};
+  }
+  if (std::strcmp(option, "--window-height") == 0) {
+    context.state = State::PARSING_VALUE;
+    context.valuePtr = &context.args->heightWindow;
+    return {};
+  }
+
+  return unexpected(error("unexpected option"));
+}
+
+static auto parseValue(Context& context, const char* value) -> expected<void, Error> {
+  auto result = parseUint32(value);
+  if (!result.has_value()) {
+    return unexpected(error("failed to parse value", result.error()));
+  }
+
+  context.state = State::PARSING_OPTION;
+  *context.valuePtr = *result;
+  context.valuePtr = nullptr;
+
+  return {};
 }
 
 auto Args::parse(
   int argc,
   const char **argv
 ) -> expected<optional<Args>, Error> {
-  Processor processor = Processor::CPU;
-  uint32_t processorCoreCount = 1;
-  uint32_t widthWindow = 800;
-  uint32_t heightWindow = 600;
-  optional<uint32_t> widthSimulation;
-  optional<uint32_t> heightSimulation;
+  Args args = {
+    .processor = Processor::CPU,
+    .processorCoreCount = 1,
+    .widthSimulation = 0,
+    .heightSimulation = 0,
+    .widthWindow = 800,
+    .heightWindow = 600
+  };
+
+  Context context = {
+    .state = State::PARSING_OPTION,
+    .args = &args,
+    .valuePtr = nullptr
+  };
 
   for (uint32_t i = 1; i < argc; ++i) {
-    auto arg = argv[i];
+    expected<void, Error> result;
 
-    if (std::strcmp(argv[i], "--help") == 0) {
-      std::cout << "cellular-automaton -w <width> -h <height>\n"
-                << "\nARGS:\n"
-                << '\t' << std::left << std::setw(15) << "--cpu [coreCount]" << " run simulation on CPU (default). Default coreCount is 1\n"
-                << '\t' << std::left << std::setw(15) << "--gpu" << " run simulation on GPU\n"
-                << '\t' << std::left << std::setw(15) << "-w, --width" << " simulation width\n"
-                << '\t' << std::left << std::setw(15) << "-h, --height" << " simulation height\n"
-                << '\t' << std::left << std::setw(15) << "--window-width" << " simulation width\n"
-                << '\t' << std::left << std::setw(15) << "--window-height" << " simulation height\n"
-                << '\t' << std::left << std::setw(15) << "--help" << " display this message\n";
-      return std::nullopt;
-    }
-
-    if (std::strcmp(argv[i], "--cpu") == 0) {
-      processor = Processor::CPU;
-      auto coreCount = parseIntValue(argc, argv, i + 1);
-      if (coreCount.has_value()) {
-        if (*coreCount <= 0) {
-          return unexpected(error("invalid cpu's coreCount"));
+    switch (context.state) {
+      case State::PARSING_OPTION: {
+        if (std::strcmp(argv[i], "--help") == 0) {
+          printHelp();
+          return std::nullopt;
         }
-        processorCoreCount = *coreCount;
-        i += 1;
+        result = parseOption(context, argv[i]);
+        break;
       }
-      continue;
-    }
-    if (std::strcmp(argv[i], "--gpu") == 0) {
-      processor = Processor::GPU;
-      continue;
+      case State::PARSING_VALUE: {
+        result = parseValue(context, argv[i]);
+        break;
+      }
+      case State::PARSING_OPTIONAL_VALUE: {
+        result = parseValue(context, argv[i]);
+        if (!result.has_value()) {
+          context.state = State::PARSING_OPTION;
+          context.valuePtr = nullptr;
+        }
+        result = {};
+        break;
+      }
     }
 
-    if (std::strcmp(argv[i], "-w") == 0 || std::strcmp(argv[i], "--width") == 0) {
-      auto width = parseIntValue(argc, argv, i + 1);
-      if (!width.has_value()) {
-        return unexpected(error("failed to parse width", width.error()));
-      }
-      widthSimulation = *width;
-      i += 1; // skip next arg since it already has been parsed
-      continue;
+    if (!result.has_value()) {
+      return unexpected(error("parsing input argument failed", result.error()));
     }
-    if (std::strcmp(argv[i], "-h") == 0 || std::strcmp(argv[i], "--height") == 0) {
-      auto height = parseIntValue(argc, argv, i + 1);
-      if (!height.has_value()) {
-        return unexpected(error("failed to parse height", height.error()));
-      }
-      heightSimulation = *height;
-      i += 1; // skip next arg since it already has been parsed
-      continue;
-    }
-
-    if (std::strcmp(argv[i], "--window-width") == 0) {
-      auto width = parseIntValue(argc, argv, i + 1);
-      if (!width.has_value()) {
-        return unexpected(error("failed to parse window width", width.error()));
-      }
-      widthWindow = *width;
-      i += 1; // skip next arg since it already has been parsed
-      continue;
-    }
-    if (std::strcmp(argv[i], "--window-height") == 0) {
-      auto height = parseIntValue(argc, argv, i + 1);
-      if (!height.has_value()) {
-        return unexpected(error("failed to parse window height", height.error()));
-      }
-      heightWindow = *height;
-      i += 1; // skip next arg since it already has been parsed
-      continue;
-    }
-
-    return unexpected(error("unexpected option"));
   }
 
-  if (!widthSimulation.has_value()) {
-    return unexpected(error("width is required value"));
+  if (args.widthSimulation == 0) {
+    return unexpected(error("width is required and must be positive"));
   }
-  if (!heightSimulation.has_value()) {
-    return unexpected(error("height is required"));
+  if (args.heightSimulation == 0) {
+    return unexpected(error("height is required and must be positive"));
   }
 
-  return Args {
-    .processor = processor,
-    .processorCoreCount = processorCoreCount,
-    .widthSimulation = *widthSimulation,
-    .heightSimulation = *heightSimulation,
-    .widthWindow = widthWindow,
-    .heightWindow = heightWindow
-  };
+  return args;
 }
