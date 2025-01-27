ea5345d8770f514391b69bbf0ce0080bbea1de05
OverworkedCriminal <tom.swirgon@gmail.com>
2025-01-27 12:03:49 +0100

    Move rules to separate header file

---
+++ src/application/simulation/rule.hpp
@@ -0,0 +1,52 @@
+#ifndef APPLICATION_SIMULATION_RULE_HPP
+#define APPLICATION_SIMULATION_RULE_HPP
+
+#include "application/simulation/cell.hpp"
+#include <array>
+#include <cstdint>
+
+namespace rule {
+
+constexpr std::array<uint8_t, 5> VERTICAL = {
+  0b00000000, // PADDING
+  0b00000000, // AIR
+  0b00011010, // SAND
+  0b00000010, // WATER_L
+  0b00000010  // WATER_R
+};
+
+constexpr std::array<uint8_t, 5> DIAGONAL = {
+  0b00000000, // PADDING
+  0b00000000, // AIR
+  0b00000010, // SAND
+  0b00000010, // WATER_L
+  0b00000010  // WATER_R
+};
+
+constexpr std::array<uint8_t, 5> HORIZONTAL = {
+  0b00000000, // PADDING
+  0b00000000, // AIR
+  0b00000000, // SAND
+  0b00010010, // WATER_L
+  0b00001010  // WATER_R
+};
+
+constexpr std::array<int32_t, 5> HORIZONTAL_DIRECTIONS = {
+   0, // PADDING
+   0, // AIR
+   0, // SAND
+  -1, // WATER_L
+   1  // WATER_R
+};
+
+constexpr std::array<uint8_t, 5> HORIZONTAL_OPPOSITE_DIRECTION_CELL = {
+  cell::PADDING, // PADDING
+  cell::AIR,     // AIR
+  cell::SAND,    // SAND
+  cell::WATER_R, // WATER_L
+  cell::WATER_L  // WATER_R
+};
+
+}
+
+#endif
